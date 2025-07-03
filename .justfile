edfe6a6be322ae9e23a56b6038d4deb40f9a2cca
darshanCommits <kumawatdarshan.1304@gmail.com>
2025-07-03 13:25:47 +0530

    feat(ch3.3): health_check

---
+++ .justfile
@@ -0,0 +1,2 @@
+req arg:
+    curlie -v http://127.0.0.1:3000/{{arg}}
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,30 @@
-fn main() {
-    println!("Hello, world!");
+use axum::{http::StatusCode, routing::get, Router};
+
+#[tokio::main]
+async fn main() {
+    let port = 3000;
+    let app = Router::new()
+        .route("/health_check", get(health_check));
+
+    let listener = match tokio::net::TcpListener::bind(format!("127.0.0.1:{port}")).await {
+        Ok(x) => x,
+        Err(err) => {
+            eprintln!("{err}\nSomething went wrong. Unable to bind to port {port}");
+            return;
+        }
+    };
+
+    match axum::serve(listener, app).await {
+        Ok(()) => {
+            println!("Listening on port {port}");
+        }
+        Err(err) => {
+            eprintln!("{err}\nUnable to serve at {port}");
+            return;
+        }
+    };
+}
+
+async fn health_check() -> StatusCode {
+    StatusCode::OK
 }
