89b7d50841f3e0deebf9917f9ef9ff67cb500e3b
kumawatdarshan <kumawatdarshan.1304@gmail.com>
2025-11-19 23:50:32 +0530

    logging(server): use anyhow

--- crates/server/src/main.rs
+++ crates/server/src/main.rs
@@ -1,3 +1,4 @@
+use anyhow::{Context, Result};
 use routes::get_router;
 use settings::get_configuration;
 use sqlx::SqlitePool;
@@ -5,12 +6,14 @@ use state::{AppState, create_email_client};
 use tokio::net::TcpListener;
 
 #[tokio::main]
-async fn main() -> std::io::Result<()> {
-    telemetry::init_tracing()?;
-    let config = get_configuration().expect("Failed to read Configuration");
+async fn main() -> Result<()> {
+    telemetry::init_tracing().context("Failed to initialize tracing.")?;
+    let config = get_configuration().context("Failed to read Configuration.")?;
 
     let base_url = format!("{}:{}", config.application.host, config.application.port);
-    let listener = TcpListener::bind(&base_url).await?;
+    let listener = TcpListener::bind(&base_url)
+        .await
+        .context(format!("Failed to bind to address: {base_url}"))?;
 
     let pool = SqlitePool::connect_lazy_with(config.database.options());
 
@@ -24,5 +27,9 @@ async fn main() -> std::io::Result<()> {
 
     let router = get_router(app_state);
 
-    axum::serve(listener, router).await
+    axum::serve(listener, router)
+        .await
+        .context("Failed to serve application using axum")?;
+
+    Ok(())
 }
